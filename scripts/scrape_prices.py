c89b52c1897cee91d224616c8dae031900bdfaee
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-12-27 23:45:11 -0300

    refactor(cli-helper): moved input function to utils

--- scripts/scrape_prices.py
+++ scripts/scrape_prices.py
@@ -1,46 +1,34 @@
 from playwright.sync_api import sync_playwright
-from utils.comparajogos import comparaJogosUrl
+from utils.urls import comparaJogosUrl
+from utils.cli_helpers import ask_running_option
 from pages.game_page import GamePage
 from pages.home_page import HomePage
 
 
 with sync_playwright() as p:
-    option = int(input("Qual tipo de execução você deseja?\n1 - Headed\n2 - Headless\n"))
-    while (option != 1 and option != 2):
-        print("Opção inválida")
-        option = int(input("Qual tipo de execução você deseja?\n1 - Headed\n2 - Headless\n"))
-
-    runOption = True
-    if option == 1:
-        runOption = False
-    
+    runOption = ask_running_option()
     browser = p.chromium.launch(
         headless=runOption
-    )
+    ) # Headed ou Headless dependendo da opção escolhida
+
     page = browser.new_page()
     page.goto(comparaJogosUrl)
-    #
-    # 
-    # 0. Encontrar a seção de Populares da Semana
-    homePage = HomePage(page)
+    homePage = HomePage(page) # 0. Encontrar a seção de Populares da Semana
+
     for i in range(homePage.get_cards().count()):
         card = homePage.get_cards().nth(i)
         link = card.locator("a")
-        # 1.1 Extraia o nome dele 
-        homePage.get_game_name(card)
-        # 1.2 Entre na página dele
+        homePage.get_game_name(card) # 1. Extraia o nome do jogo
         p = browser.new_page(base_url=comparaJogosUrl)
         url = link.get_attribute("href")
         if url is None:
             p.close()
-        p.goto(url)
-        #
-        #
+        p.goto(url)  # 2. Visite a página do jogo
+        
         gamePage = GamePage(p)
-        # 1.3 Extraia valores de produtos novos
-        gamePage.new_price_values()
-        # 1.4 Extraia valores de produtos usados
-        gamePage.used_price_values()
+        gamePage.new_price_values() # 3. Extraia valores (pix e crédito) do jogo novo 
+        gamePage.used_price_values() # 4. Extraia valores (pix e crédito) do jogo usado
         print("================")
         p.close()
+
     browser.close()
