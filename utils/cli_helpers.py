2dbd7a03344886f43f2e9b675629444f0699f969
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-12-28 23:08:01 -0300

    chore: removed pycache

--- utils/cli_helpers.py
+++ utils/cli_helpers.py
@@ -1,5 +1,5 @@
 def ask_running_option():
-    option = int(input("Qual tipo de execução você deseja?\n1 - Headed\n2 - Headless\n"))
+    option = int(input("Qual tipo de execução você deseja?\n1 - Headed\n2 - Headless\nSua opção: "))
     while (option != 1 and option != 2):
         print("Opção inválida")
         option = int(input("Qual tipo de execução você deseja?\n1 - Headed\n2 - Headless\n"))
